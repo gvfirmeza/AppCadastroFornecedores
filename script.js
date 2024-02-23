6e2194c7fb626fa71c3528ae618843f1e5afd117
gvfirmeza <gvfirmeza@gmail.com>
2024-02-23 09:20:03 -0300

    contruindo função para anexos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let contadorProdutos = 1;
 
 function calcularValorTotal(input) {
-    const row = input.closest('.produto'); // Encontra o elemento pai com a classe 'produto'
+    const row = input.closest('.produto');
     const quantidadeEstoque = parseFloat(row.querySelector('input[name="quantidadeEstoque[]"]').value);
     const valorUnitario = parseFloat(row.querySelector('input[name="valorUnitario[]"]').value);
     const valorTotalInput = row.querySelector('input[name="valorTotal[]"]');
@@ -22,7 +22,7 @@ function adicionarProduto() {
             <br/>
             <div class="row">
                 <div class="col-md-1 d-flex align-items-center justify-content-end">
-                <button type="button" class="btn btn-danger btnRemoverProduto mx-2 button-lixeira">
+                <button type="button" class="btn btn-danger btnRemoverProduto mx-2 p-1 button-lixeira">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -32,7 +32,7 @@ function adicionarProduto() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
                     <path fill="#fff" d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
                 </svg>
-            </div>                   
+            </div>
                 </div>
                 <div class="col-md-11">
                 <h4>Produto ${contadorProdutos}</h4>
@@ -44,9 +44,10 @@ function adicionarProduto() {
                             </div>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row my-3">
                         <div class="col-md-3">
                             <div class="form-group">
+                            <label for="unidadeMedida" class="form-label">Unidade de Medida:</label>
                             <select name="unidadeMedida[]" required class="form-select">
                                 <option value="cm">cm</option>
                                 <option value="mm">mm</option>
@@ -74,11 +75,11 @@ function adicionarProduto() {
                 </div>
             </div>
         </div>
-    </div>    
+    </div>
     `;
 
     tabelaProdutos.appendChild(divProduto);
-  
+
     divProduto.querySelectorAll('input[type="number"]').forEach((input) => {
       input.addEventListener("input", () => {
         calcularValorTotal(divProduto);
@@ -93,45 +94,32 @@ function adicionarProduto() {
     });
 }
 
-document
-  .getElementById("btnAdicionarProduto")
-  .addEventListener("click", adicionarProduto);
+function adicionarAnexoHTML(fileName, fileURL) {
+  return `
+    <li>
+      <span>${fileName}</span>
+      <a href="${fileURL}" download>Visualizar</a>
+      <button class="btnRemoverAnexo">Excluir</button>
+    </li>
+  `;
+}
 
-document.getElementById("anexo").addEventListener("change", function (event) {
-  const fileList = event.target.files;
+document.getElementById("btnAdicionarAnexo").addEventListener("click", function () {
+  const fileList = document.getElementById("anexo").files;
   const anexosList = document.getElementById("anexos");
 
   for (let i = 0; i < fileList.length; i++) {
-    const listItem = document.createElement("li");
-    listItem.textContent = fileList[i].name;
-
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Excluir";
-    deleteButton.addEventListener("click", function () {
-      listItem.remove();
-    });
-
-    listItem.appendChild(deleteButton);
-    anexosList.appendChild(listItem);
+    const file = fileList[i];
+    const fileName = file.name;
+    const fileURL = URL.createObjectURL(file);
+    const anexoHTML = adicionarAnexoHTML(fileName, fileURL);
+    anexosList.innerHTML += anexoHTML;
   }
 });
 
-document.getElementById("anexo").addEventListener("change", function (event) {
-  const fileList = event.target.files;
-  const anexosList = document.getElementById("anexos");
-
-  for (let i = 0; i < fileList.length; i++) {
-    const listItem = document.createElement("li");
-    listItem.textContent = fileList[i].name;
-
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Excluir";
-    deleteButton.addEventListener("click", function () {
-      listItem.remove();
-    });
-
-    listItem.appendChild(deleteButton);
-    anexosList.appendChild(listItem);
+document.getElementById("anexos").addEventListener("click", function (event) {
+  if (event.target.classList.contains("btnRemoverAnexo")) {
+    event.target.parentNode.remove();
   }
 });
 
